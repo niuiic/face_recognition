0289956350197719c642b2c2dca3a7a8647403f2
niuiic <niuiicde@gmail.com>
2021-03-23 22:22:32 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# face_recognition
+# Face Recognition
+
+A face recognition project based on the Ascend, refer to the official routines, the purpose is to learn the framework.
+
---
+++ build/intermediates/host/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "/home/niuiic/Documents/Project/Cpp/face_recognition/out/main"
+  "/home/niuiic/Documents/Project/Cpp/face_recognition/out/main.pdb"
+  "CMakeFiles/main.dir/camera.cpp.o"
+  "CMakeFiles/main.dir/dvpp_cropandpaste.cpp.o"
+  "CMakeFiles/main.dir/dvpp_jpegd.cpp.o"
+  "CMakeFiles/main.dir/dvpp_jpege.cpp.o"
+  "CMakeFiles/main.dir/dvpp_process.cpp.o"
+  "CMakeFiles/main.dir/dvpp_resize.cpp.o"
+  "CMakeFiles/main.dir/face_detection.cpp.o"
+  "CMakeFiles/main.dir/face_feature_mask.cpp.o"
+  "CMakeFiles/main.dir/face_post_process.cpp.o"
+  "CMakeFiles/main.dir/face_recognition.cpp.o"
+  "CMakeFiles/main.dir/face_register.cpp.o"
+  "CMakeFiles/main.dir/facial_recognition_message.pb.cc.o"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/mind_camera.cpp.o"
+  "CMakeFiles/main.dir/model_process.cpp.o"
+  "CMakeFiles/main.dir/resource_load.cpp.o"
+  "CMakeFiles/main.dir/utils.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
